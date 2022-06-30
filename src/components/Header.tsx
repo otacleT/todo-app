cfab88c78e977e7cc34e6676e97e15b126eecfce
MIYABETaisei <mytspe46@gmail.com>
2022-06-30 11:55:40 +0900

    ⚡️ ルートへのリンク作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
 import { AiFillGithub, AiFillTwitterCircle } from "react-icons/ai";
 
 const Header = () => {
-    return (
-      <header className="w-full shadow-md shadow-black/25">
-        <div className="h-[70px] max-w-3xl px-3 mx-auto  flex justify-between items-center">
-          <h1>
-            <Image
-              src="/logo.png"
-              layout="intrinsic"
-              objectFit="contain"
-              width={95}
-              height={40}
-            />
-          </h1>
-          <nav>
-            <ul className="flex justify-between">
-              <li className="px-3">
-                <a
-                  href="https://github.com/MIYABETaisei"
-                  target="_black"
-                  rel="noopenner"
-                  className="text-3xl"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="px-3">
-                <a
-                  href="https://twitter.com/otacleT"
-                  target="_black"
-                  rel="noopenner"
-                  className="text-3xl"
-                >
-                  <AiFillTwitterCircle />
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
-}
+  return (
+    <header className="w-full shadow-md shadow-black/25">
+      <div className="h-[70px] max-w-3xl px-3 mx-auto  flex justify-between items-center">
+        <h1>
+          <Link href="/">
+            <a>
+              <Image
+                src="/logo.png"
+                layout="intrinsic"
+                objectFit="contain"
+                width={95}
+                height={40}
+              />
+            </a>
+          </Link>
+        </h1>
+        <nav>
+          <ul className="flex justify-between">
+            <li className="px-3">
+              <a
+                href="https://github.com/MIYABETaisei"
+                target="_black"
+                rel="noopenner"
+                className="text-3xl"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="px-3">
+              <a
+                href="https://twitter.com/otacleT"
+                target="_black"
+                rel="noopenner"
+                className="text-3xl"
+              >
+                <AiFillTwitterCircle />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
