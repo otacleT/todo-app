e37524a4c0cc60a39d7ec2e03d5d7ad0aca66169
MIYABETaisei <mytspe46@gmail.com>
2022-06-30 11:56:02 +0900

    ✨ dndkitテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,6 @@ import Head from "next/head";
 import Link from "next/link";
 import { ChangeEventHandler } from "react";
 import { useState } from "react";
-import Draggable from "src/components/Draggable";
-import Droppable from "src/components/Droppable";
 import Header from "src/components/Header";
 
 type Todo = {
@@ -17,13 +15,6 @@ type Todo = {
 const Home: NextPage = () => {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
-  // const containers = ["Don't", "Doing", "Did"];
-  // const [parent, setParent] = useState(null);
-  // const draggableMarkup = (
-  //   <Draggable id="draggable" parent="Don't">
-  //     Drag me
-  //   </Draggable>
-  // );
   const handleInput: ChangeEventHandler<HTMLInputElement> = (e) => {
     setText(e.target.value);
   };
@@ -33,13 +24,6 @@ const Home: NextPage = () => {
     });
     setText("");
   };
-  // const handleDragEnd = (event: any) => {
-  //   const { over } = event;
-
-  // If the item is dropped over a container, set it as the parent
-  // otherwise reset the parent to `null`
-  //   setParent(over ? over.id : null);
-  // };
   return (
     <div>
       <Head>
@@ -51,7 +35,9 @@ const Home: NextPage = () => {
       <main>
         <h2 className="text-3xl font-bold text-center mt-5">Todo App</h2>
         <Link href="/test">
-          <a className="text-xl font-bold mt-4 ">Draggable test</a>
+          <a className="text-xl font-bold mt-4 mx-auto flex justify-center items-center w-[170px] h-[45px] text-center border-2 border-black box-border">
+            Draggable test
+          </a>
         </Link>
         <div className="flex justify-between max-w-6xl mx-auto px-3 mt-10">
           <div className="w-[20%]">
@@ -69,28 +55,6 @@ const Home: NextPage = () => {
               追加
             </button>
           </div>
-          {/* <DndContext onDragEnd={handleDragEnd}>
-            <div className="flex justify-between w-[calc(80%-20px)] border-2 border-black rounded-xl py-1 px-[9px]">
-              <div className="w-[calc(100%/3-6px)]">
-                <h3 className="text-xl font-bold text-center">Don't</h3>
-                <Droppable key="Don't" item="Don't">
-                  {parent === "Don't" ? draggableMarkup : "Drop here"}
-                </Droppable>
-              </div>
-              <div className="w-[calc(100%/3-6px)]">
-                <h3 className="text-xl font-bold text-center">Doing</h3>
-                <Droppable key="Doing" item="Doing">
-                  {parent === "Doing" ? draggableMarkup : "Drop here"}
-                </Droppable>
-              </div>
-              <div className="w-[calc(100%/3-6px)]">
-                <h3 className="text-xl font-bold text-center">Did</h3>
-                <Droppable key="Did" item="Did">
-                  {parent === "Did" ? draggableMarkup : "Drop here"}
-                </Droppable>
-              </div>
-            </div>
-          </DndContext> */}
           <div className="flex justify-between w-[calc(80%-20px)] border-2 border-black rounded-xl py-1 px-[9px]">
             <div className="w-[calc(100%/3-6px)]">
               <h3 className="text-xl font-bold text-center">Don't</h3>
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,27 +1,80 @@
-import React, { useState } from "react";
+import React, { ChangeEventHandler, useState } from "react";
 import { DndContext } from "@dnd-kit/core";
-import Draggable from "src/components/Draggable";
-import Droppable from "src/components/Droppable";
+import { Draggable } from "src/components/Draggable";
+import { Droppable } from "src/components/Droppable";
+import Header from "src/components/Header";
+import Head from "next/head";
 
-function Test() {
-  const containers = ["A", "B", "C", "D"];
+type Todo = {
+  id: number;
+  label: string;
+  isDone: boolean;
+};
+
+const Test = () => {
+  const containers = ["A", "B", "C"];
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo>();
+  // const [todos, setTodos] = useState<Todo[]>([]);
   const [parent, setParent] = useState(null);
-  const draggableMarkup = <Draggable id="draggable">Drag me</Draggable>;
+  const draggableMarkup = <Draggable id="draggable" label={todos?.label} />;
 
+  const handleInput: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setText(e.target.value);
+  };
+  // const handleAdd = () => {
+  //   setTodos((prevTodos) => {
+  //     return [...prevTodos, { id: Math.random(), label: text, isDone: false }];
+  //   });
+  //   setText("");
+  // };
+  const handleAdd = () => {
+    setTodos({ id: Math.random(), label: text, isDone: false });
+    setText("");
+  };
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {parent === null ? draggableMarkup : null}
-
-      <div className="max-w-3xl flex justify-between mx-auto flex-wrap">
-        {containers.map((id) => (
-          // We updated the Droppable component so it would accept an `id`
-          // prop and pass it to `useDroppable`
-          <Droppable key={id} id={id}>
-            {parent === id ? draggableMarkup : "Drop here"}
-          </Droppable>
-        ))}
-      </div>
-    </DndContext>
+    <div>
+      <Head>
+        <title>Todo App</title>
+        <meta name="description" content="Todo App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main>
+        <h2 className="text-3xl font-bold text-center mt-5">Todo App</h2>
+        <DndContext onDragEnd={handleDragEnd}>
+          <div className="flex justify-between items-start max-w-6xl mx-auto px-3 mt-10">
+            <div className="w-[20%]">
+              <h3 className="text-xl font-bold text-center">Add List</h3>
+              <input
+                className="w-full border border-black p-2 mt-3 rounded-sm"
+                value={text}
+                onChange={handleInput}
+                type="text"
+              />
+              <button
+                onClick={handleAdd}
+                className="w-full mt-5 h-10 flex items-center justify-center text-md font-bold rounded-sm text-white bg-black"
+              >
+                追加
+              </button>
+            </div>
+            <div className="w-[calc(80%-10px)] flex justify-between items-start flex-wrap max-w-6xl mx-auto px-3 mt-10">
+              <div className="w-full">
+                {parent === null ? draggableMarkup : null}
+              </div>
+              {containers.map((id) => (
+                // We updated the Droppable component so it would accept an `id`
+                // prop and pass it to `useDroppable`
+                <Droppable key={id} id={id}>
+                  {parent === id ? draggableMarkup : "Drop here"}
+                </Droppable>
+              ))}
+            </div>
+          </div>
+        </DndContext>
+      </main>
+    </div>
   );
 
   function handleDragEnd(event: any) {
@@ -31,6 +84,6 @@ function Test() {
     // otherwise reset the parent to `null`
     setParent(over ? over.id : null);
   }
-}
+};
 
 export default Test;
