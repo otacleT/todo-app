d67404c503dfdb68c9bde01876728abb7af7116b
MIYABETaisei <mytspe46@gmail.com>
2022-07-02 10:34:50 +0900

    ♻️

--- src/component/Todo.tsx
+++ src/component/Todo.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler, useState } from "react";
+import React, { useState } from "react";
 import {
   DndContext,
   DragOverlay,
@@ -7,39 +7,26 @@ import {
   PointerSensor,
   useSensor,
   useSensors,
+  DragOverEvent,
+  DragStartEvent,
+  UniqueIdentifier,
 } from "@dnd-kit/core";
 import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
-import Container from "src/components/Container";
-import { Item } from "src/components/SortableItem";
+import Container from "src/component/Container";
+import Addtodo from "./Addtodo";
+import Dragitem from "./Dragitem";
 
 type Props = {
   [key: string]: string[];
 };
 
-type Todo = {
-  id: number;
-  label: string;
-  isDone: boolean;
-};
-
-export default function App() {
+const Todo = () => {
   const [items, setItems] = useState<Props>({
-    root: ["1", "2", "3"],
-    container1: ["4", "5", "6"],
-    container2: ["7", "8", "9"],
+    container1: [],
+    container2: [],
     container3: [],
   });
-  const [text, setText] = useState("");
-  const [activeId, setActiveId] = useState();
-  const handleInput: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setText(e.target.value);
-  };
-  const handleAdd = () => {
-    setItems((prevItems) => {
-      return { ...prevItems, root: text };
-    });
-    setText("");
-  };
+  const [activeId, setActiveId] = useState<UniqueIdentifier | null>();
 
   //   入力方法の定義
   const sensors = useSensors(
@@ -50,36 +37,28 @@ export default function App() {
   );
 
   return (
-    <div className="flex row">
-      <input
-        className="w-full border border-black p-2 mt-3 rounded-sm"
-        value={text}
-        onChange={handleInput}
-        type="text"
-      />
-      <button
-        onClick={handleAdd}
-        className="w-full mt-5 h-10 flex items-center justify-center text-md font-bold rounded-sm text-white bg-black"
-      >
-        追加
-      </button>
-      <DndContext
-        sensors={sensors}
-        collisionDetection={closestCorners}
-        onDragStart={handleDragStart}
-        onDragOver={handleDragOver}
-        onDragEnd={handleDragEnd}
-      >
-        <Container id="root" items={items.root} />
-        <Container id="container1" items={items.container1} />
-        <Container id="container2" items={items.container2} />
-        <Container id="container3" items={items.container3} />
-        <DragOverlay>{activeId ? <Item id={activeId} /> : null}</DragOverlay>
-      </DndContext>
+    <div className="flex justify-between max-w-6xl mx-auto px-3 mt-10">
+      <Addtodo setItems={setItems} />
+      <div className="flex justify-between w-[calc(80%-20px)] border-2 border-black rounded-xl py-1 px-[9px]">
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCorners}
+          onDragStart={handleDragStart}
+          onDragOver={handleDragOver}
+          onDragEnd={handleDragEnd}
+        >
+          <Container label="Don't" id="container1" items={items.container1} />
+          <Container label="Doing" id="container2" items={items.container2} />
+          <Container label="Did" id="container3" items={items.container3} />
+          <DragOverlay>
+            {activeId ? <Dragitem id={activeId} /> : null}
+          </DragOverlay>
+        </DndContext>
+      </div>
     </div>
   );
 
-  function findContainer(id) {
+  function findContainer<T>(id: T) {
     if (id in items) {
       return id;
     }
@@ -87,21 +66,22 @@ export default function App() {
     return Object.keys(items).find((key) => items[key].includes(id));
   }
   //   掴んだ要素をアクティブにする
-  function handleDragStart(event) {
+  function handleDragStart(event: DragStartEvent) {
     const { active } = event;
     const { id } = active;
 
     setActiveId(id);
   }
 
-  function handleDragOver(event) {
-    const { active, over, draggingRect } = event;
+  function handleDragOver(event: DragOverEvent) {
+    // const { active, over, draggingRect } = event;
+    const { active, over } = event;
     const { id } = active;
     const { id: overId } = over;
 
     // Find the containers
-    const activeContainer = findContainer(id);
-    const overContainer = findContainer(overId);
+    const activeContainer = findContainer<UniqueIdentifier>(id);
+    const overContainer = findContainer<any>(overId);
 
     if (
       !activeContainer ||
@@ -116,7 +96,7 @@ export default function App() {
       const overItems = prev[overContainer];
 
       // Find the indexes for the items
-      const activeIndex = activeItems.indexOf(id);
+      const activeIndex = activeItems.indexOf(active.id);
       const overIndex = overItems.indexOf(overId);
 
       let newIndex;
@@ -124,10 +104,7 @@ export default function App() {
         // We're at the root droppable of a container
         newIndex = overItems.length + 1;
       } else {
-        const isBelowLastItem =
-          over &&
-          overIndex === overItems.length - 1 &&
-          draggingRect.offsetTop > over.rect.offsetTop + over.rect.height;
+        const isBelowLastItem = over && overIndex === overItems.length - 1;
 
         const modifier = isBelowLastItem ? 1 : 0;
 
@@ -148,9 +125,9 @@ export default function App() {
     });
   }
 
-  function handleDragEnd(event) {
+  function handleDragEnd(event: DragOverEvent) {
     const { active, over } = event;
-    const { id } = active;
+    const id: any = active;
     const { id: overId } = over;
 
     const activeContainer = findContainer(id);
@@ -164,7 +141,7 @@ export default function App() {
       return;
     }
 
-    const activeIndex = items[activeContainer].indexOf(active.id);
+    const activeIndex = items[activeContainer].indexOf(id);
     const overIndex = items[overContainer].indexOf(overId);
 
     if (activeIndex !== overIndex) {
@@ -180,4 +157,5 @@ export default function App() {
 
     setActiveId(null);
   }
-}
+};
+export default Todo;
