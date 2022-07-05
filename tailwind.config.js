53ac5ca2cf383131aa23d7bdc16870f1215d208c
MIYABETaisei <mytspe46@gmail.com>
2022-07-05 21:12:14 +0900

    ✨ mantineの導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1400px",
+    },
     extend: {},
   },
   plugins: [],
