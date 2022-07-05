84b6c78e6d35aca96118b322cb2c5d240c748274
MIYABETaisei <mytspe46@gmail.com>
2022-07-05 21:12:39 +0900

    ✨ datepickerの作成

--- src/component/Addschejule/AddSchejule.tsx
+++ src/component/Addschejule/AddSchejule.tsx
@@ -1,4 +1,34 @@
+import { DatePicker } from "@mantine/dates";
+import dayjs from "dayjs";
+import { ChangeEventHandler } from "react";
+import { BsCalendar } from "react-icons/bs";
+
+type Props = { date: Date };
+
 /**@package */
-export const AddSchejule = () => {
-  return <h5>aiueo</h5>;
+export const AddSchejule = ({
+  setDate,
+}: {
+  setDate: React.Dispatch<React.SetStateAction<Date>>;
+}) => {
+  const handleDate = (e: Date) => {
+    console.log(dayjs(e).format("YYYY-MM-DD"));
+    setDate(e);
+  };
+  return (
+    <div className="flex justify-between">
+      <p className="w-[80px] text-md flex justify-around items-center text-lg">
+        <BsCalendar />
+        <span className="text-sm">期限</span>
+      </p>
+      <DatePicker
+        className="w-[calc(100%-90px)]"
+        classNames={{ input: "rounded-none" }}
+        placeholder="日付を選択"
+        inputFormat="YYYY-MM-DD"
+        onChange={handleDate}
+        required
+      />
+    </div>
+  );
 };
