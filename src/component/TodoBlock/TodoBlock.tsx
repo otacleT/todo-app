56b612d07bf927ed1caa85e8a9bdc5a1baf2ae31
MIYABETaisei <mytspe46@gmail.com>
2022-07-09 16:49:05 +0900

    :art:

--- src/component/TodoBlock/TodoBlock.tsx
+++ src/component/TodoBlock/TodoBlock.tsx
@@ -1,17 +1,16 @@
 import { UniqueIdentifier } from "@dnd-kit/core";
-import dayjs from "dayjs";
 import { FC } from "react";
 
 type Props = {
-  item?: { id: UniqueIdentifier; title: string; date: Date };
+  title: UniqueIdentifier;
 };
 
 /**@package */
 export const TodoBlock: FC<Props> = (props) => {
+  const { title } = props;
   return (
     <button className="text-md font-bold block border-2 border-gray-300 shadow-lg shadow-black/30 w-full p-2 rounded-md">
-      {props.item?.title}
-      <span>{dayjs(props.item?.date).format("YYYY-MM-DD")}</span>
+      {title}
     </button>
   );
 };
