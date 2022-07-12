9017c5d9850c513d6d6a4b96e81df3988cc6ce22
MIYABETaisei <mytspe46@gmail.com>
2022-07-12 17:41:03 +0900

    ⚡️ 外観の調整 #4

--- src/component/Addtodo/AddTodo.tsx
+++ src/component/Addtodo/AddTodo.tsx
@@ -1,7 +1,7 @@
 import { UniqueIdentifier } from "@dnd-kit/core";
 import { ColorPicker } from "@mantine/core";
 import { ChangeEventHandler, FC, useState } from "react";
-import { AddSchejule } from "../Addschejule";
+import { DatePicker } from "@mantine/dates";
 
 type Props = {
   [key: string]: {
@@ -49,25 +49,34 @@ export const AddTodo: FC<TodoInput> = (props) => {
   const handleColor = (e: string) => {
     setColor(e);
   };
+  const handleDate = (e: Date) => {
+    setDate(e);
+  };
 
   return (
     <div className="w-[20%]">
       <h3 className="text-xl font-bold text-center">Todoリストを追加</h3>
+      <p className="text-[14px] font-bold">title</p>
       <input
-        className="w-full border border-[#ced4da] leading-[34px] mt-3 px-[12px] rounded-sm text-[14px]"
+        className="w-full border border-[#ced4da] leading-[34px] px-[12px] rounded-sm text-[14px]"
         value={text}
         onChange={handleInput}
         type="text"
         placeholder="勉強する"
       />
-      <div className="flex justify-between items-center mt-2">
-        <AddSchejule setDate={setDate} />
-      </div>
+      <p className="text-[14px] font-bold mt-3">date</p>
+      <DatePicker
+        classNames={{ input: "rounded-none" }}
+        placeholder="日付を選択"
+        inputFormat="YYYY-MM-DD"
+        onChange={handleDate}
+        clearable={true}
+        required
+      />
+      <p className="text-[14px] font-bold mt-3">color</p>
       <ColorPicker
         format="hex"
         swatches={[
-          "#25262b",
-          "#868e96",
           "#fa5252",
           "#e64980",
           "#be4bdb",
@@ -75,17 +84,16 @@ export const AddTodo: FC<TodoInput> = (props) => {
           "#4c6ef5",
           "#228be6",
           "#15aabf",
-          "#12b886",
-          "#40c057",
           "#82c91e",
           "#fab005",
           "#fd7e14",
         ]}
         onChange={handleColor}
       />
+      <p className="text-center text-md">{color}</p>
       <button
         onClick={handleAdd}
-        className="w-full mt-5 h-10 flex items-center justify-center text-md font-bold rounded-sm text-white bg-black"
+        className="w-full mt-5 h-10 flex items-center justify-center text-md font-bold rounded-full text-white bg-black"
       >
         追加
       </button>
