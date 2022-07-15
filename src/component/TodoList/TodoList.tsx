7cffd1aab4ce3a2aa5e3f833bd93ffb2f86ba44f
MIYABETaisei <mytspe46@gmail.com>
2022-07-15 22:04:29 +0900

    ✨ handleDeleteを渡すよう修正 #15

--- src/component/TodoList/TodoList.tsx
+++ src/component/TodoList/TodoList.tsx
@@ -1,11 +1,12 @@
-import { UniqueIdentifier, useDroppable } from '@dnd-kit/core';
-import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
-import React, { FC } from 'react';
-import { TodoItem } from '../TodoItem';
+import { UniqueIdentifier, useDroppable } from "@dnd-kit/core";
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import React, { FC } from "react";
+import { TodoItem } from "../TodoItem";
 
 type Props = {
   id: string;
   label: string;
+  handleDelete: (id: UniqueIdentifier) => void;
   items: {
     id: UniqueIdentifier;
     title: string;
@@ -16,16 +17,16 @@ type Props = {
 
 /**@package */
 export const TodoList: FC<Props> = (props) => {
-  const { id, label, items } = props;
+  const { id, label, items, handleDelete } = props;
   const { setNodeRef } = useDroppable({
     id,
   });
 
   return (
-    <div className='w-[calc(33%-5px)]'>
-      <h3 className='text-xl font-bold text-center'>{label}</h3>
+    <div className="w-[calc(33%-5px)]">
+      <h3 className="text-xl font-bold text-center">{label}</h3>
       <SortableContext id={id} items={items} strategy={verticalListSortingStrategy}>
-        <div ref={setNodeRef} className='w-full border-2 border-gray-500/75 p-5 mt-2 rounded-md'>
+        <div ref={setNodeRef} className="w-full border-2 border-gray-500/75 p-5 mt-2 rounded-md">
           {items.map((item) => (
             <TodoItem
               key={item.id}
@@ -33,6 +34,7 @@ export const TodoList: FC<Props> = (props) => {
               title={item.title}
               date={item.date}
               color={item.color}
+              handleDelete={handleDelete}
             />
           ))}
         </div>
