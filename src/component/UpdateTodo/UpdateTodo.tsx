6b9e6b958ae5fed08b67c2a112d7e524d498a479
MIYABETaisei <mytspe46@gmail.com>
2022-07-16 13:07:51 +0900

    ✨ updateTodoを作成 #16

---
+++ src/component/UpdateTodo/UpdateTodo.tsx
@@ -0,0 +1,104 @@
+import React, { ChangeEventHandler, FC, useCallback, useEffect, useState } from "react";
+import { DatePicker } from "@mantine/dates";
+import { ColorPicker } from "@mantine/core";
+import { IoCloseOutline } from "react-icons/io5";
+import { UniqueIdentifier } from "@dnd-kit/core";
+
+type Props = {
+  visible: boolean;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  id: UniqueIdentifier | undefined;
+  title: string | undefined;
+  date: Date | null | undefined;
+  color: string | undefined;
+  handleText: React.ChangeEventHandler<HTMLInputElement>;
+  handleDate: (e: Date) => void;
+  handleColor: (e: string) => void;
+  handleUp: (
+    id: UniqueIdentifier | undefined,
+    title: string | undefined,
+    date: Date | null | undefined,
+    color: string | undefined,
+  ) => void;
+};
+
+export const UpdateTodo: FC<Props> = (props) => {
+  const {
+    visible,
+    setVisible,
+    id,
+    title,
+    date,
+    color,
+    handleText,
+    handleDate,
+    handleColor,
+    handleUp,
+  } = props;
+  return (
+    <>
+      <div
+        onClick={() => setVisible(!visible)}
+        className="w-full h-full fixed top-0 left-0 bg-black/30"
+      ></div>
+      <div className="w-2/3 max-w-[400px] shadow-lg shadow-black/30 fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-white px-3 pt-6 pb-3 rounded-sm">
+        <span
+          onClick={() => setVisible(!visible)}
+          className="text-2xl absolute right-2 top-2 cursor-pointer"
+        >
+          <IoCloseOutline />
+        </span>
+        <p className="text-[14px] font-bold">
+          title<span className="text-rose-500">*</span>
+        </p>
+        <input
+          className="w-full border border-[#ced4da] leading-[34px] px-[12px] rounded-sm text-[14px]"
+          type="text"
+          value={title}
+          placeholder="勉強する"
+          onChange={handleText}
+        />
+        <p className="text-[14px] font-bold mt-3">
+          date<span className="text-rose-500">*</span>
+        </p>
+        <DatePicker
+          classNames={{ input: "rounded-none" }}
+          placeholder="日付を選択"
+          inputFormat="YYYY-MM-DD"
+          clearable={true}
+          value={date}
+          onChange={handleDate}
+          required
+        />
+        <p className="text-[14px] font-bold mt-3">color</p>
+        <ColorPicker
+          format="hex"
+          swatches={[
+            "#fa5252",
+            "#e64980",
+            "#be4bdb",
+            "#7950f2",
+            "#4c6ef5",
+            "#228be6",
+            "#15aabf",
+            "#82c91e",
+            "#fab005",
+            "#fd7e14",
+          ]}
+          onChange={handleColor}
+        />
+        {color ? (
+          <p className="text-center text-md font-[14px] leading-[34px] border-[#ced4da] border mt-2">
+            {color}
+          </p>
+        ) : null}
+        <button
+          className="w-full mt-5 h-10 flex items-center justify-center text-md font-bold rounded-full text-white bg-black"
+          onClick={() => handleUp(id, title, date, color)}
+        >
+          変更する
+        </button>
+      </div>
+    </>
+  );
+};
