706dda6abd0611022d81d4b7f8ff85014e99e8d2
MIYABETaisei <mytspe46@gmail.com>
2022-07-16 23:36:40 +0900

    ♻️ componentに分ける

--- src/component/UpdateTodo/UpdateTodo.tsx
+++ src/component/UpdateTodo/UpdateTodo.tsx
@@ -3,21 +3,24 @@ import { DatePicker } from "@mantine/dates";
 import { ColorPicker } from "@mantine/core";
 import { IoCloseOutline } from "react-icons/io5";
 import { UniqueIdentifier } from "@dnd-kit/core";
+import { ColorPick } from "../ColorPicker";
+import { DatePick } from "../DatePick";
+import { InputTitle } from "../InputTitle";
 
 type Props = {
   id: UniqueIdentifier | undefined;
-  title: string | undefined;
-  date: Date | null | undefined;
-  color: string | undefined;
+  title: string;
+  date: Date | null;
+  color: string;
   setIsShow: React.Dispatch<React.SetStateAction<boolean>>;
   handleText: React.ChangeEventHandler<HTMLInputElement>;
   handleDate: (e: Date) => void;
   handleColor: (e: string) => void;
   handleChange: (
     id: UniqueIdentifier | undefined,
-    title: string | undefined,
-    date: Date | null | undefined,
-    color: string | undefined,
+    title: string,
+    date: Date | null,
+    color: string,
   ) => void;
 };
 
@@ -37,51 +40,9 @@ export const UpdateTodo: FC<Props> = (props) => {
         >
           <IoCloseOutline />
         </span>
-        <p className="text-[14px] font-bold">
-          title<span className="text-rose-500">*</span>
-        </p>
-        <input
-          className="w-full border border-[#ced4da] leading-[34px] px-[12px] rounded-sm text-[14px]"
-          type="text"
-          value={title}
-          placeholder="勉強する"
-          onChange={handleText}
-        />
-        <p className="text-[14px] font-bold mt-3">
-          date<span className="text-rose-500">*</span>
-        </p>
-        <DatePicker
-          classNames={{ input: "rounded-none" }}
-          placeholder="日付を選択"
-          inputFormat="YYYY-MM-DD"
-          clearable={true}
-          value={date}
-          onChange={handleDate}
-          required
-        />
-        <p className="text-[14px] font-bold mt-3">color</p>
-        <ColorPicker
-          format="hex"
-          swatches={[
-            "#fa5252",
-            "#e64980",
-            "#be4bdb",
-            "#7950f2",
-            "#4c6ef5",
-            "#228be6",
-            "#15aabf",
-            "#82c91e",
-            "#fab005",
-            "#fd7e14",
-          ]}
-          value={color}
-          onChange={handleColor}
-        />
-        {color ? (
-          <p className="text-center text-md font-[14px] leading-[34px] border-[#ced4da] border mt-2">
-            {color}
-          </p>
-        ) : null}
+        <InputTitle text={title} handleInput={handleText} />
+        <DatePick date={date} handleDate={handleDate} />
+        <ColorPick color={color} handleColor={handleColor} />
         <button
           className="w-full mt-5 h-10 flex items-center justify-center text-md font-bold rounded-full text-white bg-black"
           onClick={() => handleChange(id, title, date, color)}
