d41fe2a9b19c883d40c6250c76fb6e39afc16c57
MIYABETaisei <mytspe46@gmail.com>
2022-07-16 23:35:10 +0900

    ♻️ DatePickコンポーネントを作成

---
+++ src/component/DatePick/DatePick.tsx
@@ -0,0 +1,27 @@
+import { DatePicker } from "@mantine/dates";
+import { FC } from "react";
+
+type Props = {
+  date: Date | null;
+  handleDate: (e: Date) => void;
+};
+
+export const DatePick: FC<Props> = (props) => {
+  const { date, handleDate } = props;
+  return (
+    <>
+      <p className="text-[14px] font-bold mt-3">
+        date<span className="text-rose-500">*</span>
+      </p>
+      <DatePicker
+        classNames={{ input: "rounded-none" }}
+        placeholder="日付を選択"
+        inputFormat="YYYY-MM-DD"
+        onChange={handleDate}
+        clearable={true}
+        value={date}
+        required
+      />
+    </>
+  );
+};
