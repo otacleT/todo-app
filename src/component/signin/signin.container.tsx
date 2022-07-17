237ce881e9cb5cfcd5b6dc7ab55a0b570fcd9a16
山下純一 <metarusword1998@gmail.com>
2022-07-18 00:18:21 +0900

    CalenderAPI接続 #23

--- src/component/signin/signin.container.tsx
+++ src/component/signin/signin.container.tsx
@@ -1,21 +1,21 @@
-import { EmailAuthProvider } from 'firebase/auth';
-import { auth } from 'firebaseui';
-import { FunctionComponent, useState, useEffect } from 'react';
-import { Signin as SigninComponent } from './signin';
+import { EmailAuthProvider, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "firebaseui";
+import { FunctionComponent, useState, useEffect } from "react";
+import { Signin as SigninComponent } from "./signin";
 
 export const Signin: FunctionComponent = () => {
   const [config, setConfig] = useState<auth.Config>();
 
   useEffect(() => {
     const uiConfig: auth.Config = {
-      signInFlow: 'popup',
+      signInFlow: "popup",
       signInOptions: [
         EmailAuthProvider.PROVIDER_ID,
         // FacebookAuthProvider.PROVIDER_ID,
-        // GoogleAuthProvider.PROVIDER_ID,
+        GoogleAuthProvider.PROVIDER_ID,
         // TwitterAuthProvider.PROVIDER_ID,
       ],
-      signInSuccessUrl: '/',
+      signInSuccessUrl: "/",
     };
     setConfig(uiConfig);
   }, []);
