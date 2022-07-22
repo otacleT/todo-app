57853c84ec91cea4e7ae5e6b16459d3084d80f98
MIYABETaisei <mytspe46@gmail.com>
2022-07-22 15:45:17 +0900

    🔥

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,62 +1,29 @@
-import { getAuth, signOut } from 'firebase/auth';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { FunctionComponent, useCallback } from 'react';
-import { useAuthState } from './hooks/authentication';
+import Image from "next/image";
+import Link from "next/link";
+import { FunctionComponent } from "react";
 
 /**@package */
 export const Header: FunctionComponent = () => {
-  const { isSignedIn, userName } = useAuthState();
-  const router = useRouter();
-
-  const handleSignin = useCallback(() => {
-    router.push('/signin');
-  }, [router]);
-
-  const handleSignOut = useCallback(() => {
-    signOut(getAuth());
-    router.push('/signin');
-  }, [router]);
-
   return (
-    <header className='w-full shadow-md shadow-black/25'>
-      <div className='h-[70px] max-w-3xl px-3 mx-auto  flex justify-between items-center'>
-        <Link href='/'>
-          <a className='text-2xl font-bold'>Todo App</a>
+    <header className="w-full shadow-md shadow-black/25">
+      <div className="h-[70px] max-w-3xl px-3 mx-auto  flex justify-between items-center">
+        <Link href="/">
+          <a className="text-2xl font-bold">Todo App</a>
         </Link>
         <nav>
-          <ul className='flex justify-between items-center'>
-            <li className='px-3'>
-              <Link href='/'>
-                <a target='_black' rel='noopenner'>
+          <ul className="flex justify-between items-center">
+            <li className="px-3">
+              <Link href="/">
+                <a target="_black" rel="noopenner">
                   <Image
-                    className='rounded-full'
-                    src='/demo.png'
+                    className="rounded-full"
+                    src="/demo.png"
                     width={40}
                     height={40}
-                    alt='demo'
+                    alt="demo"
                   />
                 </a>
               </Link>
-            </li>
-            <li className='px-3'>
-              <div>{userName}</div>
-            </li>
-            <li className='px-3'>
-              <div>
-                {isSignedIn ? (
-                  <>
-                    <button className='text-3md' onClick={handleSignOut}>
-                      ログアウト
-                    </button>
-                  </>
-                ) : (
-                  <button className='text-3md' onClick={handleSignin}>
-                    ログイン
-                  </button>
-                )}
-              </div>
             </li>
           </ul>
         </nav>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import { Header } from '../component/Header';
-import '../../Firebase/firebase';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Header } from "../component/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Todo App</title>
-        <meta name='description' content='Todo App' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Todo App" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Component {...pageProps} />
